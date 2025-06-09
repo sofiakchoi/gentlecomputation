b5350edb19137a40ab1a22bf977bd4f85e16cbd6
Sofia Choi <sofiakchoi@gmail.com>
2025-06-08 21:27:19 -0700

    meow

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 document.addEventListener('scroll', function() {
   var scrollPosition = window.pageYOffset;
-  var zoomLevel = Math.min(3, 1 + scrollPosition / 35);
+  var zoomLevel = Math.min(2, 0.5 + scrollPosition / 35);
 
   var zoomText = document.getElementById('zoom-text');
   zoomText.style.transform = 'translate(-50%, -50%) scale(' + zoomLevel + ')';
 });
 
+document.getElementById('hover01').addEventListener('mouseout', function() {
+  document.getElementById('img01').style.display = 'none';
+});
+
+document.getElementById('hover01').addEventListener('mouseover', function() {
+  document.getElementById('img01').style.display = 'block';
+});
