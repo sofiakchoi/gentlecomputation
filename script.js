0525a03468879818fcaabe7ca6c1fbc851a4767f
Sofia Choi <sofiakchoi@gmail.com>
2025-06-11 04:11:54 -0700

    updates

--- script.js
+++ script.js
@@ -6,6 +6,21 @@ document.addEventListener('scroll', function() {
   zoomText.style.transform = 'translate(-50%, -50%) scale(' + zoomLevel + ')';
 });
 
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
 document.getElementById('hover01').addEventListener('mouseout', function() {
   document.getElementById('img01').style.display = 'none';
 });
